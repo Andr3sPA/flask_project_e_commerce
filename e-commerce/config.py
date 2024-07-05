ae715b49c2981fdf0bfc58c70c4471619aeac7a9
Andres Darío Peña Asprilla <82219767+Andr3sPA@users.noreply.github.com>
2024-07-04 21:55:47 -0500

    Add files via upload

---
+++ e-commerce/config.py
@@ -0,0 +1,8 @@
+import os
+import secrets
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BREVO_API_KEY = os.environ.get('BREVO_API_KEY')
