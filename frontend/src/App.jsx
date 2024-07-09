217a97dd0ec976879e5a7d53a69b4cb6d79230af
Andr3sPA <andres.pena2@udea.edu.co>
2024-07-08 19:06:45 -0500

    test:jwt in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,47 +3,34 @@ import { Button } from "@/components/ui/button"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-axios.defaults.withCredentials = true;
 
 function App() {
-    // usestate for setting a javascript
-    // object for storing and using data
-    const [data, setdata] = useState({
-        name: "",
-        age: 0,
-        programming: "",
-    });
-
-    // Using useEffect for single rendering
+    const [data, setData] = useState(null);
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyMDQ3Njg1OCwianRpIjoiYjIzY2RhYmEtZDVjOS00Zjg3LWI2YzEtYWVmODhmMzZjMDNiIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFuZHJlc2RhcmlvLjIwMDFAZ21haWwuY29tIiwibmJmIjoxNzIwNDc2ODU4LCJjc3JmIjoiNDlhNGQ1NWItZDhlMC00MDAzLTkxYjktNWVmNGU3NzE2YjJiIiwiZXhwIjoxNzIwNDc3NzU4fQ.gl_Ct1yFtciR1b0jHuJIZT8RuZXFqheNSuzur7CtmWYeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyMDQ4MDQwOCwianRpIjoiZTU2Mjg2NTgtODNkOC00N2Y4LWE2MWYtZmI4ZTBiNDM1NTA3IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFuZHJlc2RhcmlvLjIwMDFAZ21haWwuY29tIiwibmJmIjoxNzIwNDgwNDA4LCJjc3JmIjoiOGY3ODRmMWYtOTUzNC00YTc5LTllNzctODMzYjdjMjhkNjE3IiwiZXhwIjoxNzIwNDg0MDA4fQ.IXM-KJ2Av2DE4sH14FdLqqJYcH8_-ztD1yUeVnPSWS4';  // Tu token de acceso aquí
+  
     useEffect(() => {
-        // Using axios to fetch the api from 
-        // flask server it will be redirected to proxy
-        axios.get("http://localhost:5000//data")
-            .then((response) => {
-                // Setting a data from api
-                setdata({
-                    name: response.data.Name,
-                    age: response.data.Age,
-                    programming: response.data.programming,
-                });
-            })
-            .catch((error) => {
-                console.error("There was an error fetching the data!", error);
-            });
+      axios.get('http://localhost:5000/protected', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
     }, []);
-
+  
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>React and Flask</h1>
-                {/* Calling a data from setdata for showing */}
-                <p>{data.name}</p>
-                <p>{data.age}</p>
-                <p>{data.programming}</p>
-            </header>
-            <Button>Button</Button>
-        </div>
+      <div>
+        {data ? (
+          <div>Logged in as: {data.logged_in_as}</div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     );
-}
+  };
 
 export default App;
