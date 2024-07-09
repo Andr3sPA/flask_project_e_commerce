91bff5a902ae1eb8a32b6b7c25312d4d7dc3afbf
Andr3sPA <andres.pena2@udea.edu.co>
2024-07-08 19:38:55 -0500

    test:protect endpoint with jwt in react and flask

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,19 @@
 import jwt
 from flask import redirect, url_for, render_template, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
-from flask_jwt_extended.jwt_manager import JWTManager
-from flask_login import logout_user, login_required, current_user
 import redis
 from __init__ import create_app, ACCESS_EXPIRES
 from flask_cors import CORS
 app = create_app()
 CORS(app)
-jwt_redis_blocklist = redis.StrictRedis(
-    host="localhost", port=6379, db=0, decode_responses=True
-)
+
 
 with app.app_context():
     from models import db, User
 
     db.create_all()  # Crear todas las tablas
-# Callback function to check if a JWT exists in the redis blocklist
-@jwt.token_in_blocklist_loader
-def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
-    jti = jwt_payload["jti"]
-    token_in_redis = jwt_redis_blocklist.get(jti)
-    return token_in_redis is not None
-# Register a callback function that loads a user from your database whenever
-# a protected route is accessed. This should return any python object on a
-# successful lookup, or None if the lookup failed for any reason (for example
-# if the user has been deleted from the database).
-@jwt.user_lookup_loader
-def user_lookup_callback(_jwt_header, jwt_data):
-    identity = jwt_data["sub"]
-    return User.query.filter_by(id=identity).one_or_none()
-@app.route("/logout", methods=["DELETE"])
-@jwt_required()
-def logout():
-    jti = get_jwt()["jti"]
-    jwt_redis_blocklist.set(jti, "", ex=ACCESS_EXPIRES)
-    return jsonify(msg="Access token revoked")
+
+
 @app.route('/register', methods=["POST"])
 def register():
     import auth
